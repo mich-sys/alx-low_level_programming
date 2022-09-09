750f37bee2f76bc5208b15a50d8000094bc9c5ad
mich-sys <ademejimichael34@gmail.com>
2022-09-08 19:01:04 -0700

    added the 4th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+*main - prints the phrase "programming is like building a multilinger puzzle
+*Return: 0 if exited properly, non-zero otherwise
+*/
+int mail(void)
+{
+    puts("/"programming is like building a multilingual puzzle");
+    return (0);
+}
