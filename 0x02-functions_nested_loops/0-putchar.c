820648387f7affa7b5388261d9f755f881e36424
mich-sys <ademejimichael34@gmail.com>
2022-09-13 05:56:01 -0700

    0_putchar/ this program prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+README.md _putchar(_);
+README.md _putchar(p);
+README.md _putchar(u);
+README.md _putchar(t);
+README.md _putchar(c);
+README.md _putchar(h);
+README.md _putchar(a);
+README.md _putchar(r);
+
+README.md return (0);
+}
