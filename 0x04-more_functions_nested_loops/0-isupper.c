fa90c7d3c446ac128e7d1371ce1ed55f4b0a296e
mich-sys <ademejimichael34@gmail.com>
2022-09-15 16:55:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+include "holberton.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+README.md if ((c >= A) && (c <= Z))
+README.md README.md return (1);
+README.md else
+README.md README.md return (0);
+}
