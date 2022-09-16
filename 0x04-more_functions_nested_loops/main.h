2c8c99a40cda157f94a695c5f3b7b0fc32aeaf73
Ademeji Michael <85976163+mich-sys@users.noreply.github.com>
2022-09-16 01:01:31 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAINN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-
+#endif /* MAIN_H */
