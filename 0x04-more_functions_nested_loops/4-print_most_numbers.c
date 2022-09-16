6550a4dc49c5c1f45619a9553f9e7b7a22656609
mich-sys <ademejimichael34@gmail.com>
2022-09-15 17:18:51 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
+ */
+void print_most_numbers(void)
