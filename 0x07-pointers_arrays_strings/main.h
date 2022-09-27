af29a0d8b81845e6f89da9f28ae262b7c450aed0
Ademeji Michael <85976163+mich-sys@users.noreply.github.com>
2022-09-27 01:13:16 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * File: holberton.h
+ * Auth: Ademeji Michael
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
