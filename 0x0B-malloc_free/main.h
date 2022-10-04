57f5bfb61b1e179937efd41a5687b3e6a62df42b
mich-sys <ademejimichael34@gmail.com>
2022-10-04 16:51:41 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,16 @@
+nes (10 sloc)  309 Bytes
+
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif
+Footer
+© 2022 GitHub, In
